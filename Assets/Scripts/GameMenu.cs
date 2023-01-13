a29f4b7d889909010ab3ee05c6bf11dab1d5ae65
Sergey Dyakov <s.dyakov9@gmail.com>
2023-01-13 15:08:32 +0300

    In Game menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+
+    [SerializeField] GameObject panel;
+    [SerializeField] GameObject background;
+    [SerializeField] PauseManager pauseManager;
+    [SerializeField] GameObject playerStatsUI;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(panel.activeInHierarchy == false) {
+                OpenMenu();
+            } else {
+                CloseMenu();
+            }
+        }
+    }
+
+    public void CloseMenu() {
+        panel.SetActive(false);
+        background.SetActive(false);
+        playerStatsUI.SetActive(true);
+        pauseManager.UnpauseGame();
+    }
+
+    public void OpenMenu() {
+        playerStatsUI.SetActive(false);
+        background.SetActive(true);
+        panel.SetActive(true);
+        pauseManager.PauseGame();
+    }
+
+    public void BackToMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
